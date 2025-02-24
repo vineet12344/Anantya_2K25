ba91c34b86c2d05410f577af17af1baca6aade86
parth <parth@parthjoshi2973@gmail.com>
2025-02-24 22:36:29 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 import "./App.css";
+import { useState,useEffect } from "react";
 import HomePage from "./components/home/home2/Homepage";
 import Navbar from "./components/home/navbar/navbar";
 import Footer from "./components/home/footer/footer";
 import CollegeEvents from "./components/events page/EventPage.jsx";
-import  CosmicAnimation from "./components/gallery/back.jsx";
-import  TeamGrid from "./components/Team/team.jsx";
+import CosmicAnimation from "./components/gallery/back.jsx";
+import TeamGrid from "./components/Team/team.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Homepage from './components/home/home2/Homepage';
-import CalenderPage from './components/calender-page/calenderpage.jsx';
+import CalenderPage from "./components/calender-page/calenderpage.jsx";
+import Loader from "./components/Loader/Loader2.jsx";
 
 // Hello
 
-
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(!loading);
+    }, 6900);
+  }, []);
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/events" element={<CollegeEvents />} />
-        <Route path="/gallery" element={<CosmicAnimation/>} />
-        <Route path="/calender" element={<CalenderPage/>} />
-        <Route path="/team" element={< TeamGrid />} />
-       
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      {loading?<Loader/>:<Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/events" element={<CollegeEvents />} />
+          <Route path="/gallery" element={<CosmicAnimation />} />
+          <Route path="/calender" element={<CalenderPage />} />
+          <Route path="/team" element={<TeamGrid />} />
+        </Routes>
+        <Footer />
+      </Router>}
+    </>
   );
 }
 
